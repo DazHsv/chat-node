90de901ba4cc94c755e8ab577aa3ad07727c2dda
Hugo Sanchez V <programacionhsv@gmail.com>
2016-06-17 00:56:47 -0500

    Iniciado

---
+++ public/app.js
@@ -0,0 +1,25 @@
+var socket = io();
+var chat = document.querySelector('#chat');
+angular.module('Chat',[])
+  .controller('MessageCtrl',function($scope,$http){
+    $scope.messages = [];
+    $scope.message = "";
+    $scope.sendMsg = function(){
+      if($scope.message != ""){
+        var msg = {
+          body:$scope.message,
+          date: new Date()
+        };
+
+        socket.emit('message',msg);
+        $scope.message = "";
+      }
+    };
+
+    socket.on('message', function(msg){
+      $scope.$apply(function(){
+        $scope.messages.push(msg);
+        chat.scrollTop = chat.style.height;
+      });
+    });
+  });
---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var http = require('http');
+var app = express();
+var server = http.Server(app);
+var io = require('socket.io')(server);
+
+app.set('view engine', 'ejs');
+app.use('/a', express.static( __dirname.concat('/public/') ));
+
+app.get('/',function(req,res){
+    res.render('index');
+});
+
+io.on('connection',function(socket){
+    io.emit('message', {
+        date: new Date(),
+        body: "User in"
+    });
+
+    socket.on('message',function(msg){
+        io.emit('message',msg);
+    });
+
+    socket.on('disconnect',function(){
+        io.emit('message', {
+            date: new Date(),
+            body: "User disconnected"
+        });
+    });
+});
+
+server.listen(8080);
